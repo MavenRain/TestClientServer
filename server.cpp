9aa9e6e62311f9d6ddbb59f6837d4a63f73ffc70
U-SEA\oobi <oobi@BELMJ03TJN9.SEA.CORP.EXPECN.com>
2016-06-21 10:52:05 -0700

    Removed debug code from server

--- server.cpp
+++ server.cpp
@@ -11,19 +11,15 @@ using namespace std;
 int main(int argc, const char * * argv)
 {
 	auto listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	cout << "Socket retrieved\n";
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(8080);
 	bind(listener, reinterpret_cast<struct sockaddr *>(&address), sizeof(address));
-	cout << "Socket bound\n";
 	listen(listener, 10);
-	cout << "Socket listening\n";
 	while(1)
 	{
 		auto client = accept(listener, nullptr, nullptr);
-		cout << "Client connected\n";
 		thread { [&client]
 		{
 			while(1)
